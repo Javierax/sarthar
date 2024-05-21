4901564ae99b55bd86d55d415f85f45d796cdac8
Javierax <153627942+Javierax@users.noreply.github.com>
2024-05-21 19:55:31 +0530

    login css

--- src/components/pages/LoginSignup.jsx
+++ src/components/pages/LoginSignup.jsx
@@ -7,9 +7,9 @@ const LoginSignup = () => {
       <div className="loginsignup-container">
         <h1>Sign up</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name' />
-          <input type="email" placeholder='email Address' />
-          <input type="password" placeholder='Password'/>
+          <input type="text" placeholder='Your Name' name="Your Name" required/>
+          <input type="email" placeholder='email Address' required />
+          <input type="password" placeholder='Password' required/>
         </div>
         <button>Continue</button>
         <p className="loginsignup-login">Already have an Account? <span>Login here</span></p>
